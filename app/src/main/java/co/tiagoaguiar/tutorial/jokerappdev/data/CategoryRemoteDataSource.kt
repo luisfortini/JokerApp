2832cbd3da9734b74accd63fab53cbfca8a92ee1
Luis Fortini <luisfortini12@gmail.com>
2022-11-22 14:50:29 -0300

    Aula 22/11/2022

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/data/CategoryRemoteDataSource.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/data/CategoryRemoteDataSource.kt
@@ -2,18 +2,40 @@ package co.tiagoaguiar.tutorial.jokerappdev.data
 
 import android.os.Handler
 import android.os.Looper
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.create
 
 class CategoryRemoteDataSource() {
 
-    fun findAllCategories(callback: ListCategoryCallback){
-        Handler(Looper.getMainLooper()).postDelayed({
-            val response = arrayListOf(
-                "Categoria 1",
-                "Categoria 2"
-            )
-            callback.onSuccess(response)
-            //onError("FALHA NA CONEXÃO. TENTE NOVAMENTE MAIS TARDE")
-            callback.onComplete()
-        }, 4000)
+    fun findAllCategories(callback: ListCategoryCallback) {
+        HTTPClient.retrofit()
+            .create(ChuckNorrisAPI::class.java)
+            .findAllCategories()
+            .enqueue(object : Callback<List<String>> {
+                override fun onResponse(
+                    call: Call<List<String>>,
+                    response: Response<List<String>>
+                ) {
+
+                    if (response.isSuccessful) {
+                        val categories = response.body()
+                        callback.onSuccess(categories ?: emptyList())
+                    } else {
+                        val error = response.errorBody()?.string()
+                        callback.onError(error ?: "Erro Desconhecido")
+                    }
+
+                    callback.onComplete()
+                }
+
+                override fun onFailure(call: Call<List<String>>, t: Throwable) {
+                    callback.onError(t.message ?: "Erro Interno")
+                    callback.onComplete()
+                }
+
+
+            })
     }
 }
